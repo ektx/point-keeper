e0b6ccc5d0f17a53e3fbfaef64ec491008e06b91
wenlong <530675800@qq.com>
2026-04-22 21:12:15 +0800

    update

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,14 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routers = [
-    {
-        path: '/',
-        name: 'root',
-        component: () => import('../src/App.vue')
-    }
-]
+// const routers = [
+//     {
+//         path: '/',
+//         name: 'root',
+//         component: () => import('../src/App.vue')
+//     }
+// ]
 
-export default createRouter({
-    history: createWebHashHistory('point-keeper'),
-    routes: routers
-})
+// export default createRouter({
+//     history: createWebHashHistory('point-keeper'),
+//     routes: routers
+// })
